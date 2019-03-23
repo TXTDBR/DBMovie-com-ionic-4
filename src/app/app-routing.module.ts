679e19b6cc5e68cbe62190a031d61e8c1d9f0874
jean dos santos <jeanddg@hotmail.com>
2019-03-22 21:16:02 -0300

    fist commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
+  { path: 'movie-details', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule' }
 ];
 
 @NgModule({
@@ -23,4 +16,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,9 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Movie',
+      url: '/movies',
+      icon: 'film'
     }
   ];
 
