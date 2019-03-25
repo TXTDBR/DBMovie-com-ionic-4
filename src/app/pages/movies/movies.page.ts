a2168af36b5dab8c5eb9024e65ab840182ede982
jean dos santos <jeanddg@hotmail.com>
2019-03-25 20:54:53 -0300

    adicionando provider service

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderService } from 'src/app/services/provider.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
+  providers: [
+    ProviderService
+  ]
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  constructor(private mdbService: ProviderService) { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  topRatedMovies(){
+    this.mdbService.getMovieTopRated().subscribe(
+      data=>{
+        let resposta = (data as any)._body;
+        resposta = JSON.parse(resposta);
+        console.log(resposta);
+    }, error=>{
+      console.log(error);
+    });
   }
 
 }
--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderService {
+  private URL:string = "https://api.themoviedb.org/3";
+  private API:string = "76783b6dbc60f58297b302f9bef9eb6d";
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getMovieTopRated():any{
+    return this.http.get(`${this.URL}/movie/top_rated?api_key=${this.API}`);
+  }
 }
