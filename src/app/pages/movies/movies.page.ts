b2826e23ca75612e0acaed09640532b9e776c577
jean dos santos <jeanddg@hotmail.com>
2019-03-26 21:13:40 -0300

    refazendo provider service

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProviderService } from 'src/app/services/provider.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-movies',
@@ -10,23 +11,34 @@ import { ProviderService } from 'src/app/services/provider.service';
   ]
 })
 export class MoviesPage implements OnInit {
+  
   public movies:any = [];
+  private param:string = "top_rated";
 
-  constructor(private mdbService: ProviderService) { }
+  constructor(private mdbService: ProviderService,
+    private loadingController: LoadingController) { }
   ngOnInit() { 
     this.topRatedMovies();
   }
+  
+  
 
-  topRatedMovies(){
-    this.mdbService.getMovieTopRated().subscribe(
+  async topRatedMovies(){
+    const loading = await this.loadingController.create({
+      message: 'Carregando filmes...'
+    });
+  
+    await loading.present();
+    this.mdbService.getMovies(this.param).subscribe(
       data=>{
-        let resposta = (data as any)._body;
+        /*let resposta = (data as any)._body;
         resposta = JSON.parse(resposta);
-        console.log(resposta);
-        this.movies = resposta.results;
+        console.log(resposta);*/
+        this.movies = data.results;
+        loading.dismiss();
     }, error=>{
       console.log(error);
-    });
+    }).add();
   }
 
 }
--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,9 +15,25 @@ export class ProviderService {
   private URL:string = "https://api.themoviedb.org/3";
   private API:string = "76783b6dbc60f58297b302f9bef9eb6d";
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getMovieTopRated():any{
     return this.http.get(`${this.URL}/movie/top_rated?api_key=${this.API}`);
+  }
+  //retorno do tipo observable
+  getMovies(param:string):Observable<any>{
+    const url = `${this.URL}/movie/${param}?api_key=${this.API}`;
+    return this.http.get<any>(url).pipe(
+      tap(_ => console.log(`O parametro requisitado foi: ${param}`)),
+      catchError(this.handletError<any>(`Falha no getMovies parametro = ${param}`))
+    );
+  }
+    private handletError<T>(Operator = 'Operation', result?: T){
+      return (error: any):Observable<T> =>{
+        console.log(error);//log do erro no console
+
+        //matem o app rodando por ter retornado o obj vazio
+        return of(result as T);
+      };
   }
 }
